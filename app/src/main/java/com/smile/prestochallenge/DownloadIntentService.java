f6408a78dee9237021a9b6e3221841fd1c74eef3
chaolee <leechao1959@gmail.com>
2018-09-19 03:09:07 -0400

    Initial commit

---
+++ app/src/main/java/com/smile/prestochallenge/DownloadIntentService.java
@@ -0,0 +1,121 @@
+package com.smile.prestochallenge;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class DownloadIntentService extends IntentService {
+
+    public static final String TAG = "DownloadIntentService";
+    public static final String ActionName = "DownloadIntentService";
+
+    private int result = Activity.RESULT_CANCELED;
+
+    public DownloadIntentService() {
+        super("DownloadIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        Log.d(TAG, "onHandleIntent is called.");
+        Bundle extras = intent.getExtras();
+        if (extras == null) {
+            return;
+        }
+
+        String urlPath  = extras.getString("URL_PATH");
+
+        result = Activity.RESULT_CANCELED;
+
+        InputStream iStream = null;
+        InputStreamReader iReader = null;
+        HttpsURLConnection myConnection = null;
+
+        try {
+            // use REST APIs
+            URL url = new URL(urlPath);
+            myConnection = (HttpsURLConnection)url.openConnection();
+            myConnection.setReadTimeout(15000);
+            myConnection.setConnectTimeout(15000);
+            myConnection.setRequestMethod("POST");
+            myConnection.setDoInput(true);
+            myConnection.setDoOutput(true);  // for write data to web. This method triggers POST request
+            int responseCode = myConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {  // value = 200
+                // successfully
+                Log.i(TAG, "REST Web Service -> Succeeded to connect.");
+
+                iStream = myConnection.getInputStream();
+                iReader = new InputStreamReader(iStream,"UTF-8");
+
+                StringBuilder sb = new StringBuilder("");
+                int readBuff = -1;
+                while((readBuff=iReader.read()) != -1) {
+                    sb.append((char)readBuff);
+                }
+                String flickrData = sb.toString();
+                Log.i(TAG, "Web output -> " + flickrData);
+
+                result = Activity.RESULT_OK;  // successfully downloaded
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception.");
+        } finally {
+            if (iReader != null)
+            {
+                try {
+                    iReader.close();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if (iStream != null) {
+                try {
+                    iStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (myConnection != null) {
+                // disconnect the resource
+                myConnection.disconnect();
+            }
+        }
+
+        // publish the result using sendBroadcast
+        Intent notificationIntent = new Intent(ActionName);
+
+        Bundle ex = new Bundle();
+        ex.putInt("RESULT",result);
+        notificationIntent.putExtras(ex);
+
+        sendBroadcast(notificationIntent); // this will work for global broadcast receiver
+
+        System.out.println("Finished IntentService.");
+    }
+}
---
+++ app/src/main/java/com/smile/prestochallenge/MainActivity.java
@@ -0,0 +1,214 @@
+package com.smile.prestochallenge;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ArrayList<Bitmap> imageBitmaps = new ArrayList();
+    private ArrayList<String> imageSizes = new ArrayList();
+    private ArrayList<String> imageDimensions = new ArrayList();
+    private ArrayList<String> imageTitles = new ArrayList();
+    private TextView loadingTextView;
+    private ListView listView = null;
+
+    private BroadcastReceiver receiver = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        loadingTextView = findViewById(R.id.loadingTextView);
+        loadingTextView.setVisibility(View.VISIBLE);
+
+        listView = findViewById(R.id.imageListView);
+        listView.setVisibility(View.GONE);
+
+        Button quitButton = (Button)findViewById(R.id.quitButton);
+        // okButton.setTextSize(textFontSize);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToPrevious();
+            }
+        });
+
+        receiver = new mainActivityReceiver();
+
+        Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
+        Bundle extras = new Bundle();
+        // extras.putString("URL_PATH", "https://api.flickr.com/services/rest/?api_key=949e98778755d1982f537d56236bbb42&method=flickr.photos.search");
+        // msg="Parameterless searches have been disabled. Please use flickr.photos.getRecent instead.
+        extras.putString("URL_PATH", "https://api.flickr.com/services/rest/?api_key=949e98778755d1982f537d56236bbb42&method=flickr.photos.getRecent");
+        intent.putExtras(extras);
+        startService(intent);
+        Log.d(TAG, "IntentService started.");
+    }
+
+    @Override
+    public void onBackPressed() {
+        returnToPrevious();
+    }
+
+    private void returnToPrevious() {
+        finish();
+    }
+
+    private class myListAdapter extends ArrayAdapter {  // changed name to MyListAdapter from myListAdapter
+
+        private int layoutId;
+        private ArrayList<Bitmap> images;
+        private ArrayList<String> sizes;
+        private ArrayList<String> dimensions;
+        private ArrayList<String> titles;
+
+        @SuppressWarnings("unchecked")
+        myListAdapter(Context context, int layoutId, ArrayList<Bitmap> images, ArrayList<String> sizes, ArrayList<String> dimensions, ArrayList<String> titles) {
+            super(context, layoutId, titles);
+
+            this.layoutId = layoutId;
+
+            if (images == null) {
+                this.images = new ArrayList<>();
+            } else {
+                this.images = images;
+            }
+            if (sizes == null) {
+                this.sizes = new ArrayList<>();
+            } else {
+                this.sizes = sizes;
+            }
+            if (dimensions == null) {
+                this.dimensions = new ArrayList<>();
+            } else {
+                this.sizes = dimensions;
+            }
+            if (titles == null) {
+                this.titles = new ArrayList<>();
+            } else {
+                this.titles = titles;
+            }
+        }
+
+        @Nullable
+        @Override
+        public Object getItem(int position) {
+            return super.getItem(position);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public int getPosition(@Nullable Object item) {
+            return super.getPosition(item);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            View view = getLayoutInflater().inflate(layoutId, parent,false);
+
+            if (getCount() == 0) {
+                return view;
+            }
+
+            int listViewHeight = parent.getHeight();
+            int itemNum = 4;
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                itemNum = 3;
+            }
+            int itemHeight = listViewHeight / itemNum;    // items for one screen
+            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+            layoutParams.height = itemHeight;
+            // view.setLayoutParams(layoutParams);  // no needed
+
+            ImageView showImageView = view.findViewById(R.id.showImageView);
+            TextView imageSizeTextView = view.findViewById(R.id.imageSizeTextView);
+            TextView imageDimensionTextView = view.findViewById(R.id.imageDimensionTextView);
+            TextView imageTitleTextView = view.findViewById(R.id.imageTitleTextView);
+
+            showImageView.setImageBitmap(images.get(position));
+            imageSizeTextView.setText(sizes.get(position));
+            imageDimensionTextView.setText(dimensions.get(position));
+            imageTitleTextView.setText(titles.get(position));
+
+            return view;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DownloadIntentService.ActionName);
+        registerReceiver(receiver, filter);  // use global broadcast receiver
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);    // use global broadcast receiver
+    }
+
+    private class mainActivityReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context,Intent intent) {
+            System.out.println("onReceive() is called.");
+            loadingTextView.setVisibility(View.GONE);
+            Bundle extras = null;
+            String action = intent.getAction();
+            switch (action) {
+                case DownloadIntentService.ActionName:
+                    extras = intent.getExtras();
+                    if (extras == null) {
+                        return;
+                    }
+                    loadingTextView.setVisibility(View.GONE);
+                    int result = extras.getInt("RESULT");
+                    if (result == Activity.RESULT_OK) {
+                        listView.setVisibility(View.VISIBLE);
+                        listView.setAdapter(new myListAdapter(MainActivity.this, R.layout.list_item, imageBitmaps, imageSizes, imageDimensions, imageTitles));
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        });
+                        Toast.makeText(context,"Download succeeded.",Toast.LENGTH_LONG).show();
+                    } else {
+                        // download failed
+                        Toast.makeText(context,"Download failed.",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
